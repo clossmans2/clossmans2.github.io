75e3d051ff30430a33d69178de88a4fb599356ef
clossmans2 <clossmans2@gmail.com>
2014-07-06 03:09:58 -0400

    Attempted to add full text search
    worked to try to include lunr and full text search, will try more again
    tomorrow.

---
+++ assets/themes/bs3-superhero/js/search.js
@@ -0,0 +1,38 @@
+---
+---
+var docs = 
+[ 
+{% for post in site.posts limit:10 %}
+  {% include themes/bs3-superhero/post.json %},
+{% endfor %}
+];
+// init lunr
+var idx = lunr(function () {
+  this.field('title', 10);
+  this.field('content');
+})
+// add each document to be index
+for(var index in docs) {
+  idx.add(docs[index]);
+}
+
+$(function() {
+	$("#searchButton").click(function() {
+		search();
+	});
+	$("#searchBox").keypress(function(e) {
+		if(e.which == 13) {
+			e.preventDefault();
+			search();
+		}
+	});
+})
+
+function search() {
+	var result = idx.search($('#searchBox').val());
+	if(result && result.length > 0) {
+		window.location.replace(result[0].ref);
+	} else {
+		alert('Found nothing');
+	}
+}
